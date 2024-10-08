184da0c66fe98828a1999fff0c941bac72b803f0
metalboyrick <rsulisthio@gmail.com>
2024-10-09 01:14:43 +0700

    feat: add deployment script

---
+++ packages/foundry/script/DeployCampaignFactory.s.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "../contracts/CampaignFactory.sol";
+import "./DeployHelpers.s.sol";
+
+contract DeployCampaignFactory is ScaffoldETHDeploy {
+    // use `deployer` from `ScaffoldETHDeploy`
+    // only deplloy CampaignFactory
+    function run() external ScaffoldEthDeployerRunner {
+        address easAddress = vm.envAddress("EAS_CONTRACT_ADDRESS");
+        address erc20Address = vm.envAddress("ERC20_CONTRACT_ADDRESS");
+
+        // use known eas and the erc20 address on base sepolia
+        CampaignFactory yourContract = new CampaignFactory(
+            easAddress,
+            erc20Address
+        );
+        console.logString(
+            string.concat(
+                "CampaignFactory deployed at: ",
+                vm.toString(address(yourContract))
+            )
+        );
+    }
+}
