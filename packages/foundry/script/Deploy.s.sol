d7f24021770c0d6216d592930fdbde62bc6a7eb0
metalboyrick <rsulisthio@gmail.com>
2024-10-09 19:01:58 +0700

    feat: add env for erc20 address

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -6,20 +6,33 @@ import {DeployCampaignFactory} from "./DeployCampaignFactory.s.sol";
 import {MockERC20} from "forge-std/mocks/MockERC20.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
+    address erc20Address;
+
     function run() external {
         // deploy fake idrx since they dont exists currently
+        erc20Address = vm.envAddress("ERC20_CONTRACT_ADDRESS");
 
         // todo: disable if already exists
-        MockERC20 mockErc20 = deployMockERC20("Mock IDRX", "IDRX", 18);
-        console.logString(
-            string.concat(
-                "Mock IDRX deployed at: ",
-                vm.toString(address(mockErc20))
-            )
-        );
+        if (erc20Address == address(0)) {
+            MockERC20 mockErc20 = deployMockERC20("Mock IDRX", "IDRX", 18);
+            erc20Address = address(mockErc20);
+            console.logString(
+                string.concat(
+                    "Mock IDRX deployed at: ",
+                    vm.toString(address(mockErc20))
+                )
+            );
+        } else {
+            console.logString(
+                string.concat(
+                    "Mock IDRX deployed at: ",
+                    vm.toString(erc20Address)
+                )
+            );
+        }
 
         // deploy campaign factory
         DeployCampaignFactory deployCampaignFactory = new DeployCampaignFactory();
-        deployCampaignFactory.run(address(mockErc20));
+        deployCampaignFactory.run(address(erc20Address));
     }
 }
